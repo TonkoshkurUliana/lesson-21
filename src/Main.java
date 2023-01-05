793d1f83e6f4171b7b21d16df24204b75dcef946
Tonkoshkur.Uliana <mikityukus@gmail.com>
2023-01-05 17:12:23 +0200

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        File file = new File("Fields.txt");
+        Method m = new Method();
+        Field[] fields = Book.class.getDeclaredFields();
+
+        List<String> annotationValues = new ArrayList<>();
+        for (int i = 0; i < fields.length; i++) {
+            Field field = fields[i];
+
+            if (field.getAnnotation(Fielder.class) instanceof Fielder) {
+                annotationValues.add(field.getName() + " -----  annotaion value = " + field.getAnnotation(Fielder.class).params());
+            }
+        }
+        m.serialize(annotationValues,file);
+        System.out.println("File: " + m.deserealize(file));
     }
 }
--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Task2 {
+        import java.time.*;
+        import java.util.Date;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        Date date = new Date();
+        System.out.println(date);
+
+        System.out.println(dataToLocalDateTime(date).toLocalDate());
+        System.out.println(dataToLocalDateTime(date).toLocalTime());
+        System.out.println(dataToLocalDateTime(date).toLocalDateTime());
+    }
+
+    public static ZonedDateTime dataToLocalDateTime(Date date) {
+
+        ZonedDateTime zdt = date.toInstant().atZone(ZoneId.systemDefault());
+
+        return zdt;
+    }
 }
